95ce9d97c12cd78c7ef5218cd8321dfc16a8bfa7
rikin88 <rikin.asher@gmail.com>
2015-10-21 12:16:45 -0500

    added files

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -6,13 +6,13 @@ angular.module("sportsStore")
 				name: "Product #1", description: "A product", 
 				category: "Category #1", price: 100 },
 			{
-				name: "Product #2", description" A product", 
+				name: "Product #2", description: "A product", 
 				category: "Category #3", price: 110 },
 			{ 
 				name: "Product #3", description: "A product", 
 				category: "Category #3", price: 210 },
 			{
-				name: "Product #4", description" A product", 
+				name: "Product #4", description: "A product", 
 				category: "Category #4", price: 202 }
 		]
 	};
